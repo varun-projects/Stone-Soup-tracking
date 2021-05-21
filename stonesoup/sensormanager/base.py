d8226f11f016363fa017f4ebc898a5dc03af7bb0
Nikki Perree <nperree@dstl.gov.uk>
2021-05-21 10:41:45 +0100

    Update sensor manager documentation

--- stonesoup/sensormanager/base.py
+++ stonesoup/sensormanager/base.py
@@ -2,7 +2,7 @@
 
 from abc import abstractmethod, ABC
 from typing import Callable, Set
-from random import sample, shuffle
+# from random import sample, shuffle
 import numpy as np
 import itertools as it
 
@@ -61,13 +61,14 @@ class RandomSensorManager(SensorManager):
     sensors: Set[Sensor] = Property(doc="The sensor(s) which the sensor manager is managing. "
                                         "These must be capable of returning available actions.")
 
-    def choose_actions(self, tracks_list, timestamp, nchoose=1, *args, **kwargs):
-        """Return a randomly chosen [list of] action(s) from the action set. To ensure no
-        order-preservation, the action set is first listified and then shuffled before a sample
-        is selected.
+    def choose_actions(self, timestamp, nchoose=1, *args, **kwargs):
+        """Returns a randomly chosen [list of] action(s) from the action set for each sensor.
 
         Parameters
         ----------
+        timestamp: :class:`datetime.datetime`
+            Time at which the actions are being chosen
+
         nchoose : int
             Number of actions from the set to choose (default is 1)
 
@@ -80,24 +81,34 @@ class RandomSensorManager(SensorManager):
 
         for sensor in self.sensors:
             actions = sensor.get_actions(timestamp)
-            sensor_action_assignment[sensor] = np.random.choice(list(actions))#, nchoose=nchoose)
+            sensor_action_assignment[sensor] = np.random.choice(list(actions))
 
         return sensor_action_assignment
 
 
 class BruteForceSensorManager(SensorManager):
     """A sensor manager which returns a choice of action from those available,
-    based on selecting the maximum reward as calculated by a reward function."""
+    selecting the option which returns the maximum reward as calculated by a reward function.
 
-    sensors:  Set[Sensor] = Property(doc="Set of sensors in use")
-    predictor: KalmanPredictor = Property()
-    updater: ExtendedKalmanUpdater = Property()
-    reward_function: Callable = Property()
+    """
+
+    sensors: Set[Sensor] = Property(doc="The sensor(s) which the sensor manager is managing. "
+                                        "These must be capable of returning available actions.")
+    predictor: KalmanPredictor = Property(doc="Predictor used to predict the track to a new state")
+    updater: ExtendedKalmanUpdater = Property(doc="Updater used in the reward function to update "
+                                                  "the track to the new state.")
+    reward_function: Callable = Property(doc="A function or class to calculate the reward "
+                                             "associated with a given configuration of sensors "
+                                             "and actions. The configuration which gives the "
+                                             "maximum value of this reward will be selected as "
+                                             "the chosen configuration of sensors and actions "
+                                             "at this time step.")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Generate a dictionary measurement models for each sensor (for use before any measurements have been made)
+        # Generate a dictionary measurement models for each sensor
+        # (for use before any measurements have been made)
         self.measurement_models = dict()
         for sensor in self.sensors:
             measurement_model = CartesianToBearingRange(
@@ -108,7 +119,23 @@ class BruteForceSensorManager(SensorManager):
             self.measurement_models[sensor] = measurement_model
 
     def choose_actions(self, tracks_list, timestamp, nchoose=1, *args, **kwargs):
-        """ """
+        """Returns a chosen [list of] action(s) from the action set for each sensor.
+        Chosen action(s) is selected by finding the configuration of sensors: actions which returns
+        the maximum reward, as calculated by a reward function.
+
+        Parameters
+        ----------
+        tracks_list: list of :class:`~Track`
+            List of tracks at given time. Used in reward function.
+        timestamp: :class:`datetime.datetime`
+            Time at which the actions are being chosen
+        nchoose : int
+            Number of actions from the set to choose (default is 1)
+
+        Returns
+        -------
+        : dict
+            The pairs of {sensor: action(s) selected}"""
         all_action_choices = dict()
 
         # For each sensor, randomly select an action to take
@@ -122,7 +149,7 @@ class BruteForceSensorManager(SensorManager):
                 relative_position = prediction.state_vector[[0, 2]] - sensor.position[[0, 1]]
                 angle_to_target = np.arctan2(relative_position[1], relative_position[0])
                 if angle_to_target in actions:
-                    action_choices.append(actions.action_from_value(angle_to_target))  # what do we want in the config?
+                    action_choices.append(actions.action_from_value(angle_to_target))
 
             all_action_choices[sensor] = action_choices
 
@@ -139,8 +166,5 @@ class BruteForceSensorManager(SensorManager):
                 selected_config = config
                 best_reward = reward
 
-        #         print(selected_config, best_reward)
         # Return mapping of sensors and chosen actions for sensors
         return selected_config
-
-
